14ee18067be58f5f8e8518060f9bf97a10b41a85
IlIEmptyIlI <132489781+IlIEmptyIlI@users.noreply.github.com>
2023-10-25 23:25:57 +0300

    Add files via upload

---
+++ src/components/head.js
@@ -0,0 +1,13 @@
+import Welcome from "./welcome.js"
+
+function Header()
+{
+    return(
+<header classname ="Header">
+<h1>header of the progect</h1>
+<Welcome name="User"/>
+</header>
+    )
+}
+
+export default Header
